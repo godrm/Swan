9f98dc301e62e95338e9d18e42a5c0bd72ccbc71
Kim Jung <godrm77@gmail.com>
2022-05-12 17:48:14 +0900

    Enhanced drawing for symbol report

--- SwanKit/SwanKit/Reporters/GraphSymbolReporter.swift
+++ SwanKit/SwanKit/Reporters/GraphSymbolReporter.swift
@@ -21,9 +21,10 @@ public struct GraphSymbolReporter: Reporter {
         var fileMap = Dictionary<String, Subgraph>()
         var objectMap = Dictionary<String, Subgraph>()
         var nodeToObjectMap = Dictionary<Node, Subgraph>()
-        var nodeMap = Dictionary<String, Node>()
+        var nodeUSRMap = Dictionary<String, Node>()
         var usrToFileMap = Dictionary<String, String>()
         var implicitMap = Dictionary<String, Symbol>()
+        var extensionMap = Dictionary<String, Subgraph>()
         var edges = Set<Edge>()
         var clusterIndex = 1
         var moduleIndex = 0
@@ -61,7 +62,9 @@ public struct GraphSymbolReporter: Reporter {
             }
             
             if selected.roles.contains(.definition) || selected.relations.count == 0  {
-                if (selected.roles.contains(.implicit) && selected.roles.contains(.definition) &&
+                if (
+                    ((selected.roles.contains(.implicit) && selected.roles.contains(.definition)) ||
+                     (selected.roles.contains(.accessorOf) && selected.roles.contains(.definition)) ) &&
                          selected.relations.count == 1 &&
                          (selected.symbol.name.hasPrefix("getter:") ||
                           selected.symbol.name.hasPrefix("setter:"))) {
@@ -92,11 +95,11 @@ public struct GraphSymbolReporter: Reporter {
                     label = "-\(selected.symbol.name)"
                 }
 
-                var node = nodeMap[selected.symbol.usr]
+                var node = nodeUSRMap[selected.symbol.usr]
                 if node == nil {
                     node = Node(selected.symbol.usr)
                     node?.label = label
-                    if selected.symbol.kind == .class || selected.symbol.kind == .enum || selected.symbol.kind == .struct {
+                    if selected.symbol.isKindOfObject() {
                         var object : Subgraph? = objectMap[selected.symbol.usr]
                         if object == nil {
                             object = Subgraph(id: "cluster_o\(clusterIndex)",
@@ -108,16 +111,16 @@ public struct GraphSymbolReporter: Reporter {
                             objectMap[selected.symbol.usr] = object
                             file?.append(object!)
                         }
+                        
+                        for relation in selected.relations where relation.symbol.kind == .extension {
+                            extensionMap[relation.symbol.usr] = object!
+                        }
                         node = nil
                         continue
                     }
                     else if selected.symbol.kind == .typealias {
                         node?.shape = .box
                         node?.textColor = Color.named(.darkviolet)
-                    }
-                    else if selected.symbol.kind == .protocol {
-                        node?.shape = .box
-                        node?.textColor = Color.named(.darkseagreen4)
                     }
                     else if selected.symbol.kind == .classProperty
                                 || selected.symbol.kind == .staticProperty
@@ -128,8 +131,7 @@ public struct GraphSymbolReporter: Reporter {
     
                     var objectMapped = false
                     for relation in selected.relations {
-                        if relation.roles.contains(.childOf) &&
-                            (relation.symbol.kind == .class || relation.symbol.kind == .enum || relation.symbol.kind == .struct) {
+                        if relation.roles.contains(.childOf) && relation.symbol.isKindOfObject() {
                             var object : Subgraph? = objectMap[relation.symbol.usr]
                             if object == nil {
                                 object = Subgraph(id: "cluster_o\(clusterIndex)",
@@ -145,13 +147,13 @@ public struct GraphSymbolReporter: Reporter {
                                 object?.append(node!)
                                 nodeToObjectMap[node!] = object
                             }
-                            nodeMap[selected.symbol.usr] = node!
+                            nodeUSRMap[selected.symbol.usr] = node!
                             objectMapped = true
                             break
                         }
                     }
                     if !objectMapped {
-                        nodeMap[selected.symbol.usr] = node!
+                        nodeUSRMap[selected.symbol.usr] = node!
                         file?.append(node!)
                         usrToFileMap[selected.symbol.usr] = selected.location.path
                     }
@@ -161,35 +163,43 @@ public struct GraphSymbolReporter: Reporter {
             
         for selected in occurrences {
             let file : Subgraph? = fileMap[selected.location.path]
-            
-            var label = "\(selected.symbol.name)"
-            if (selected.symbol.kind == .protocol) {
-                label = "<<\(selected.symbol.name)>>"
-            }
-            else if (selected.symbol.kind == .staticMethod || selected.symbol.kind == .classMethod) {
-                label = "+\(selected.symbol.name)"
-            }
-            else if (selected.symbol.kind == .instanceMethod) {
-                label = "-\(selected.symbol.name)"
-            }
-
             if selected.symbol.kind == .parameter ||
                 selected.symbol.kind == .enumConstant ||
                 selected.relations.count == 1 && selected.relations.first?.symbol.kind == .parameter ||
-                selected.relations.count == 1 && selected.relations.first?.symbol.kind == .staticProperty
-            {
+                selected.relations.count == 1 && selected.relations.first?.symbol.kind == .staticProperty {
                 continue
             }
 
             var selectedUSR = selected.symbol.usr
-            var node = nodeMap[selectedUSR]
+            var node = nodeUSRMap[selectedUSR]
             if node == nil {
+                var symbolPointer = selected.symbol
                 if let original = implicitMap[selected.symbol.usr] {
                     selectedUSR = original.usr
+                    node = nodeUSRMap[selectedUSR]
+                    symbolPointer = original
                 }
-                node = Node(selectedUSR)
-                node?.label = label + ((selected.symbol.properties.contains(.ibAnnotated) || selected.symbol.properties.contains(.ibOutletCollection)) ? "@IB" : "")
-                if selected.symbol.kind == .class || selected.symbol.kind == .enum || selected.symbol.kind == .struct || selected.symbol.kind == .typealias {
+                if node == nil {
+                    node = Node(selectedUSR)
+                }
+                var label = "\(symbolPointer.name)"
+                if (symbolPointer.kind == .protocol) {
+                    label = "<<\(symbolPointer.name)>>"
+                }
+                else if (symbolPointer.kind == .staticMethod || symbolPointer.kind == .classMethod) {
+                    label = "+\(symbolPointer.name)"
+                }
+                else if (symbolPointer.kind == .instanceMethod) {
+                    label = "-\(symbolPointer.name)"
+                }
+                else if symbolPointer.kind == .classProperty
+                            || symbolPointer.kind == .staticProperty
+                            ||  symbolPointer.kind == .instanceProperty {
+                    label = "." + label
+                }
+
+                node?.label = label + ((symbolPointer.properties.contains(.ibAnnotated) || symbolPointer.properties.contains(.ibOutletCollection)) ? "@IB" : "")
+                if symbolPointer.kind == .class || symbolPointer.kind == .enum || selected.symbol.kind == .struct || selected.symbol.kind == .typealias {
                     node?.shape = .box
                     node?.textColor = Color.named(.darkviolet)
                 }
@@ -200,15 +210,13 @@ public struct GraphSymbolReporter: Reporter {
             }
 
             for relation in selected.relations {
-                let founded = nodeMap[selectedUSR] != nil
+                let founded = nodeUSRMap[selectedUSR] != nil
                 if relation.roles.contains(.childOf) &&
-                    ( relation.symbol.kind == .struct ||
-                      relation.symbol.kind == .class ||
-                      relation.symbol.kind == .enum) {
+                    ( relation.symbol.isKindOfObject()) {
                     if founded {
                         var object : Subgraph? = objectMap[relation.symbol.usr]
                         if object == nil {
-                            object = Subgraph(id: "cluster_o\(clusterIndex)", label: relation.symbol.name)
+                            object = Subgraph(id: "cluster_o\(clusterIndex)", label: "\(relation.symbol.kind) \(relation.symbol.name)")
                             clusterIndex += 1
                             object?.textColor = Color.named(.darkseagreen4)
                             object?.borderWidth = 1
@@ -223,13 +231,18 @@ public struct GraphSymbolReporter: Reporter {
                     }
                     else {
                         file?.append(node!)
-                        nodeMap[selectedUSR] = node!
+                        nodeUSRMap[selectedUSR] = node!
                         usrToFileMap[selectedUSR] = selected.location.path
                     }
                     continue
                 }
+                else if relation.symbol.kind == .extension {
+                    if let object = extensionMap[relation.symbol.usr] {
+                        object.append(node!)
+                    }
+                    continue
+                }
                 else if relation.symbol.kind == .parameter ||
-                            relation.symbol.kind == .extension ||
                             relation.roles.contains(.overrideOf) ||
                             relation.symbol.kind == .variable && relation.roles.contains(.containedBy) {
                     continue
@@ -245,63 +258,73 @@ public struct GraphSymbolReporter: Reporter {
                 if let object = objectMap[selectedUSR], nodeToObjectMap[node!] == nil &&  selected.roles.contains(.reference) {
                     object.append(node!)
                     nodeToObjectMap[node!] = object
-                    nodeMap[selectedUSR] = node!
+                    nodeUSRMap[selectedUSR] = node!
                 }
                 else if let object = objectMap[relation.symbol.usr], relation.roles.contains(.receivedBy) {
                     if nodeToObjectMap[node!] == nil {
                         object.append(node!)
                         nodeToObjectMap[node!] = object
-                        nodeMap[relation.symbol.usr] = node!
+                        nodeUSRMap[relation.symbol.usr] = node!
                     }
                     else {
                         continue
                     }
                 }
-                else if relation.roles.contains(.receivedBy) && nodeMap[selectedUSR] == nil {
+                else if relation.roles.contains(.receivedBy) && nodeUSRMap[selectedUSR] == nil {
                     guard let recvFilePath = usrToFileMap[relation.symbol.usr],
                           let recvFile = fileMap[recvFilePath] else { continue }
-                    nodeMap[selectedUSR] = node!
+                    nodeUSRMap[selectedUSR] = node!
                     usrToFileMap[selectedUSR] = recvFilePath
                     recvFile.append(node!)
                     continue
                 }
-                else if nodeMap[selectedUSR] == nil {
+                else if nodeUSRMap[selectedUSR] == nil {
                     file?.append(node!)
-                    nodeMap[selectedUSR] = node!
+                    nodeUSRMap[selectedUSR] = node!
                     usrToFileMap[selectedUSR] = selected.location.path
                 }
 
-                var symbolName = "\(relation.symbol.name)"
-                if (relation.symbol.kind == .protocol) {
-                    symbolName = "<<\(relation.symbol.name)>>"
+                var mappedSymbol = relation.symbol
+                if relation.roles.contains(.containedBy) &&
+                    (relation.symbol.name.hasPrefix("getter:") || relation.symbol.name.hasPrefix("setter:")),
+                    let mapped = implicitMap[relation.symbol.usr] {
+                    mappedSymbol = mapped
                 }
-                var other = nodeMap[relation.symbol.usr]
+                var symbolName = "\(mappedSymbol.name)"
+                if (mappedSymbol.kind == .protocol) {
+                    symbolName = "<<\(mappedSymbol.name)>>"
+                }
+                var other = nodeUSRMap[mappedSymbol.usr]
                 if other == nil {
-                    if (relation.symbol.kind == .staticMethod || relation.symbol.kind == .classMethod) {
-                        symbolName = "+\(relation.symbol.name)"
+                    if (mappedSymbol.kind == .staticMethod || mappedSymbol.kind == .classMethod) {
+                        symbolName = "+\(mappedSymbol.name)"
                     }
-                    else if (relation.symbol.kind == .instanceMethod) {
-                        symbolName = "-\(relation.symbol.name)"
+                    else if (mappedSymbol.kind == .instanceMethod) {
+                        symbolName = "-\(mappedSymbol.name)"
                     }
-                    else if let object = objectMap[relation.symbol.usr], nodeToObjectMap[node!] == nil {
+                    else if let object = objectMap[selectedUSR], nodeToObjectMap[node!] == nil {
                         object.append(node!)
                         nodeToObjectMap[node!] = object
                         continue
                     }
-                    other = Node(relation.symbol.usr)
+                    other = Node(mappedSymbol.usr)
                     other?.label = symbolName + (selected.symbol.properties.contains(.ibAnnotated) ? "@IB" : "")
-                    if relation.symbol.kind == .class || relation.symbol.kind == .enum ||
-                                relation.symbol.kind == .struct || relation.symbol.kind == .typealias {
-                        other?.shape = .box
-                        other?.textColor = Color.named(.darkviolet)
-                    }
-                    nodeMap[relation.symbol.usr] = other
-                    if let object = objectMap[relation.symbol.usr], nodeToObjectMap[other!] == nil  {
+                    if let object = objectMap[mappedSymbol.usr], nodeToObjectMap[other!] == nil  {
                         object.append(other!)
                         nodeToObjectMap[other!] = object
                     }
+                    else if mappedSymbol.kind == .instanceMethod || mappedSymbol.kind == .staticMethod {
+                        continue
+                    }
+                    else if mappedSymbol.isKindOfObject() {
+                        continue
+//                        other?.shape = .box
+//                        other?.textColor = Color.named(.darkviolet)
+//                        systemModule.append(other!)
+                    }
+                    nodeUSRMap[mappedSymbol.usr] = other
                 }
-                                
+                
                 var edge = Edge(from: other!, to: node!)
                 guard !edges.contains(edge) else { continue }
                 edges.insert(edge)
